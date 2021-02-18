7985fd880e46c4ff11e6d4b28b32f0ffde175923
Staticyassine <55593917+Staticyassine@users.noreply.github.com>
2021-02-18 09:57:29 +0100

    name change

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/bo'));
+app.use(express.static('./dist/argon-design-system-angular'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/bo'}),
+    res.sendFile('index.html', {root: 'dist/argon-design-system-angular'}),
 );
 
 app.listen(process.env.PORT || 8080);
